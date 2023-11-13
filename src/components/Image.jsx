4f6b6aba7128bd4562c3a78366fe2d12a78cc0ce
Pritz Belleza <pritz.belleza@gmail.com>
2023-11-12 20:57:35 -0500

    update Image to use object-fit cover add width, height props to have it customized

--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -1,14 +1,14 @@
-import React from 'react';
+import './Image.css';
 import PropTypes from "prop-types";
 
-const Image = ({ src, alt, srcDesktop }) => {
+const Image = ({ src, alt, srcDesktop, width, height }) => {
   return ( 
     <img 
       className='Image'
       src={src}
       alt={alt}
-      width={380}
-      height={500}
+      width={width}
+      height={height}
       srcSet={`${src} 300w, ${srcDesktop} 1000w`}
     />
   )
@@ -17,7 +17,9 @@ const Image = ({ src, alt, srcDesktop }) => {
 Image.propTypes = {
   src: PropTypes.string,
   alt: PropTypes.string,
-  srcDesktop: PropTypes.string
+  srcDesktop: PropTypes.string,
+  width: PropTypes.number,
+  height: PropTypes.number
 }
 
 export default Image
