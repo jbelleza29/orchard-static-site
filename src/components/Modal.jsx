c943eefcc61bc3c7aa1adaa355a06e27e220c069
Pritz Belleza <pritz.belleza@gmail.com>
2023-11-13 10:16:22 -0500

    add feature where clicking outside the modal closes it

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,22 +1,27 @@
-
+import { useRef } from 'react';
 import {createPortal} from 'react-dom';
-
 import PropTypes from "prop-types";
 
+import useOnClickOutside from '../hooks/useOnClickOutside';
+
+
 export default function Modal({ isOpen, onClose, imageSrc, alt }) {
+  const modalElement = useRef(null);
+  useOnClickOutside(modalElement, onClose);
+
   if(!isOpen){
     return null;
   }
- return createPortal(
-   <div className='modal'>
-       <div className='modal-container'>
-           <div className='modal-body'>
-              <img src={imageSrc} alt={alt} width={380} height={600} />
-           </div>
-           <button onClick={onClose}>Close</button>
-       </div>
-    
-   </div>
+
+  return createPortal(
+    <div className='modal'>
+      <div className='modal-container' ref={modalElement}>
+        <button className='modal-close' onClick={onClose}>X</button>
+        <div className='modal-body'>
+          <img src={imageSrc} alt={alt} width={380} />
+        </div>
+      </div>
+    </div>
    , document.getElementById('modal')
  )
 }
