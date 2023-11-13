7a8b5c551b3a4328b99fbe8f58c01777086f7b79
Pritz Belleza <pritz.belleza@gmail.com>
2023-11-13 03:21:15 -0500

    add modal component

---
+++ src/components/Modal.jsx
@@ -0,0 +1,30 @@
+
+import {createPortal} from 'react-dom';
+import './Modal.css';
+
+import PropTypes from "prop-types";
+
+export default function Modal({ isOpen, onClose, imageSrc, alt }) {
+  if(!isOpen){
+    return null;
+  }
+ return createPortal(
+   <div className='modal'>
+       <div className='modal-container'>
+           <div className='modal-body'>
+              <img src={imageSrc} alt={alt} width={380} height={600} />
+           </div>
+           <button onClick={onClose}>Close</button>
+       </div>
+    
+   </div>
+   , document.getElementById('modal')
+ )
+}
+
+Modal.propTypes = {
+  imageSrc: PropTypes.string,
+  alt: PropTypes.string,
+  isOpen: PropTypes.bool,
+  onClose: PropTypes.func
+}
