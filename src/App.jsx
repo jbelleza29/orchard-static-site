cdb5db361eb44764d1e9b2eaf53158e3f955b677
Pritz Belleza <pritz.belleza@gmail.com>
2023-11-12 22:31:48 -0500

    add content to first content block add styling to first content block

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,14 @@ import imageOne from './assets/component-01/image-01.jpg';
 import imageOne2x from './assets/component-01/image-01@2x.jpg';
 
 function App() {
-  console.log(imageOne);
+  // mock cms data
+  const block1 = {
+    title: "Answer your body's needs",
+    copy: "The way ingredients are sourced affects the way we nourish our bodies. Author Mark Schatzer believes our body naturally develops an appetite for the foods and nutrients it needs to be healthy, but that artificial flavourings are getting in the way. This can be reversed by focusing on high-quality ingredients and being mindful as your appetite guides you to consume according to your body's needs",
+    subtitle: "Be mindful",
+    subtitleCopy: "Sourcing local or organic food is a good way to start being more mindful about what you're cooking and eating" 
+  };
+
   return (
     <main className="container">
       <section className="container-body">
@@ -28,14 +35,14 @@ function App() {
             srcDesktop={imageOne2x}
             alt="father and son walking"
             width={380}
-          height={290}
+            height={290}
           />
         </div>
         <article>
-          <h2>Answer your body&apos;s needs</h2>
-          <p>The way ingredients are sourced affects the way we nourish our bodies. Author Mark Schatzer believes our body naturally develops an appetite for the foods and nutrients it needs to be healthy, but that artificial flavourings are getting in the way. This can be reversed by focusing on high-quality ingredients and being mindful as your appetite guides you to consume according to your body&apos;s needs.</p>
-          <h3>Be mindful</h3>
-          <p>Sourcing local or organic food is a good way to start being more mindful about what you&apos;re cooking and eating</p>
+          <h2>{block1.title}</h2>
+          <p>{block1.copy}</p>
+          <h3>{block1.subtitle}</h3>
+          <p>{block1.subtitleCopy}</p>
         </article>
       </section>
     </main>
